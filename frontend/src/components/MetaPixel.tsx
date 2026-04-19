9970efa022644698d5117c53d9b8393faad34640
iamm8m <malam212082@bscse.uiu.ac.bd>
2026-04-19 15:26:42 +0600

    feat: add PageView tracking event to Meta Pixel initialization

--- frontend/src/components/MetaPixel.tsx
+++ frontend/src/components/MetaPixel.tsx
@@ -45,6 +45,7 @@ export default function MetaPixel() {
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
             fbq('init', '${FB_PIXEL_ID}');
+            fbq('track', 'PageView');
           `,
         }}
       />
